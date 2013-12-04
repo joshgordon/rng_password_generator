213258b3782c67e9170f5721d49166fe4ae7300f
Josh Gordon <github@joshgordon.net>
2013-12-04 14:19:42 -0500

    Working

--- passwd.js
+++ passwd.js
@@ -1,11 +1,18 @@
 var http = require("http"); 
 var url = require("url") ;
 var RandBytes = new require("randbytes"); 
+var fs = require("fs"); 
 
 var randomSource = new RandBytes.urandom({
   filePath: "/dev/hwrng"
       });
 
+
+function pathToInt(path)
+{ 
+  var out = path.replace(/[^0-9]/g, ''); 
+  return parseInt(out, 10); 
+}
 
 function start()
 { 
@@ -13,15 +20,26 @@ function start()
   {
     var pathname = url.parse(request.url).pathname; 
     console.log("Request for " + pathname + " recived.");
+    var times = pathToInt(pathname); 
+    console.log("Looping " + times + " times"); 
 
     response.writeHead(200, {"Content-Type": "text/plain"});
+
+    var file = fs.readFileSync('./diceware8k.txt', 'utf-8'); 
+    var lines = file.split('\n'); 
     
-    var randNum;
-    RandBytes.getRandomBytes(2, function(buff) {
-	randNum = buff[0] + buff[1] * 256;});
-    response.write ("" + randNum); 
+    response.write("password: "); 
+
+    for (var i=0; i<times; i++)
+      { 
+	var randNum;
+	RandBytes.getRandomBytes(2, function(buff) {
+	    randNum = buff[0] + buff[1] * 256;});
+	response.write(lines[+randNum & 0x1fff] + " "); 
+      }
     
-    response.write("password"); 
+   
+    response.write("\n"); 
     response.end(); 
   }
 
